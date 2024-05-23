532aa45794b6b1246bbe02cf97a20f541920b45a
useEffects <joel.sr1024@gmail.com>
2024-05-24 02:21:04 +0530

    basic usage

---
+++ interpreter/main.go
@@ -0,0 +1,54 @@
+package interpreter
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+type Interpreter struct {
+	hadError bool
+}
+
+func (i *Interpreter) RunFile(path string) error {
+	bytes, err := os.ReadFile(path)
+	if err != nil {
+		return err
+	}
+
+	return i.Run(string(bytes))
+}
+
+func (i *Interpreter) RunPrompt() {
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Print("> ")
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			if err == os.ErrClosed {
+				break
+			}
+			fmt.Fprintln(os.Stderr, err)
+			continue
+		}
+		line = line[:len(line)-1]
+		if err := i.Run(line); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+	}
+}
+
+func (i *Interpreter) Run(source string) error {
+	return nil
+}
+
+func (i *Interpreter) Error(line int, message string) {
+	i.Report(line, "", message)
+}
+
+func (i *Interpreter) Report(line int, where, message string) {
+	log.Fatalln("[line", line, "] Error", where, ":", message)
+	i.hadError = true
+}
--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "golox/scanner"
+import (
+	"golox/interpreter"
+	"log"
+	"os"
+)
 
 func main() {
-	scanner.Hello()
+	interp := interpreter.Interpreter{}
+	if len(os.Args) > 1 {
+		log.Fatal("Usage: golox [script]")
+		os.Exit(64)
+	} else if len(os.Args) == 2 {
+		interp.RunFile(os.Args[1])
+	} else {
+		interp.RunPrompt()
+	}
 }
